505dff50394ccc3411447d8b3e0f74ddd6f5df36
AdityaSrivastavDS <114422969+AdityaSrivastavDS@users.noreply.github.com>
2022-10-18 00:32:00 +0530

    Delete multiplication.py

--- multiplication.py
+++
@@ -1,9 +0,0 @@
-numb = int(input(" Enter a number : "))    
-
-# using the for loop to generate the multiplication tables     
-
-print("Table of: ")  
-
-for a in range(1,11):    
-
-   print(num,'x',a,'=',num*a)  
